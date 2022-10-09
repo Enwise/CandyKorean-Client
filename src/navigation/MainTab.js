c8514dc65707f62f73d24a1157fa4988ad109fc8
sohyun <kshyun0215@gmail.com>
2022-10-09 15:39:26 +0900

    폰트 추가 후 tab에 적용

--- src/navigation/MainTab.js
+++ src/navigation/MainTab.js
@@ -2,24 +2,32 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Class from "../screens/Class";
-import MyLecture from "../screens/MyLecture";
 import Premium from "../screens/Premium";
 import MyPage from "../screens/MyPage";
 import ClassInactive from "../assets/icons/ClassInactive";
-import MyLectureInactive from "../assets/icons/MyLectureInactive";
+import ClassRoomInactive from "../assets/icons/ClassRoomInactive";
 import MyPageInactive from "../assets/icons/MyPageInactive";
 import PremiumInactive from "../assets/icons/PremiumInactive";
 import HomeInactive from "../assets/icons/HomeInactive";
-
+import { useFonts } from "expo-font";
+import ClassRoom from "../screens/ClassRoom";
 const Tab = createBottomTabNavigator();
 
 const MainTab = () => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+  if (!fontsLoaded) return null;
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-Regular",
+          },
+          tabBarActiveTintColor: "black",
           tabBarIcon: ({ focused }) => {
             return focused ? <HomeInactive /> : <HomeInactive />;
           },
@@ -29,17 +37,25 @@ const MainTab = () => {
         name="Class"
         component={Class}
         options={{
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-Regular",
+          },
+          tabBarActiveTintColor: "black",
           tabBarIcon: ({ focused }) => {
             return focused ? <ClassInactive /> : <ClassInactive />;
           },
         }}
       />
       <Tab.Screen
-        name="MyLecture"
-        component={MyLecture}
+        name="ClassRoom"
+        component={ClassRoom}
         options={{
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-Regular",
+          },
+          tabBarActiveTintColor: "black",
           tabBarIcon: ({ focused }) => {
-            return focused ? <MyLectureInactive /> : <MyLectureInactive />;
+            return focused ? <ClassRoomInactive /> : <ClassRoomInactive />;
           },
         }}
       />
@@ -47,15 +63,23 @@ const MainTab = () => {
         name="Premium"
         component={Premium}
         options={{
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-Regular",
+          },
+          tabBarActiveTintColor: "black",
           tabBarIcon: ({ focused }) => {
             return focused ? <PremiumInactive /> : <PremiumInactive />;
           },
         }}
       />
       <Tab.Screen
-        name="MyPage"
+        name="My"
         component={MyPage}
         options={{
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-Regular",
+          },
+          tabBarActiveTintColor: "black",
           tabBarIcon: ({ focused }) => {
             return focused ? <MyPageInactive /> : <MyPageInactive />;
           },
