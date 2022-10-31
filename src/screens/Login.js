ea6f25f710c00e5e0b13300be7bf7024291ea864
sohyun <kshyun0215@gmail.com>
2022-11-01 00:07:20 +0900

    로그인 후 메인페이지로 이동

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,48 @@
+import { useFonts } from "expo-font";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-const Login = () => {
+import Constants from "expo-constants";
+import { Dimensions, StyleSheet, Text, TextInput, View } from "react-native";
+import Logo from "../components/Logo";
+import SignButton from "../components/SignButton";
+const windowWidth = Dimensions.get("window").width;
+const Login = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  if (!fontsLoaded) return null;
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <View
+        style={{
+          flex: 5,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Logo />
+      </View>
+      <View style={{ flex: 2 }}>
+        <TextInput
+          style={styles.input}
+          placeholder="Please enter your email"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Please enter your password"
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+        <SignButton
+          title="LOGIN"
+          onPress={() => navigation.navigate("MainTab")}
+        />
+      </View>
     </View>
   );
 };
@@ -11,8 +50,16 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "white",
+    paddingTop: Constants.statusBarHeight,
+  },
+  input: {
+    width: windowWidth - 40,
+    padding: 10,
+    height: 40,
+    borderWidth: 1,
+    marginBottom: 20,
   },
 });
 export default Login;
