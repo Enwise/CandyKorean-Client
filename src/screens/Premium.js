19544d876a2b1643d8b4d8b00aebac6bb99f093b
sohyun <kshyun0215@gmail.com>
2022-11-10 02:41:07 +0900

    프리미엄 메인 디자인 적용

--- src/screens/Premium.js
+++ src/screens/Premium.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Dimensions,
+  Image,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -8,62 +9,74 @@ import {
 } from "react-native";
 import Constants from "expo-constants";
 import * as Progress from "react-native-progress";
+import { useFonts } from "expo-font";
+import PremiumEnter from "../assets/icons/PremiumEnter";
 const windowWidth = Dimensions.get("window").width;
 const Premium = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  if (!fontsLoaded) return null;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity style={{ marginRight: 10 }}>
-          <Text>Log out</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Setting");
+        <Text style={{ fontFamily: "Poppins-SemiBold", fontSize: 20 }}>
+          Premium Course
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Poppins-Regular",
+            fontSize: 12,
+            color: "#B8B5BC",
           }}
         >
-          <Text>setting</Text>
-        </TouchableOpacity>
+          Meet the tutor in the metaverse space
+        </Text>
       </View>
-      <View style={styles.content}>
-        <View style={styles.profile}>
-          <View style={styles.profileLeftContainer}>
-            <View style={styles.profileImg}>
-              <Text>프로필사진</Text>
-            </View>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={() => {
+          navigation.navigate("Tutoring");
+        }}
+      >
+        <View style={styles.courseContainer}>
+          <View style={{ paddingVertical: 15 }}>
+            <Image
+              source={require("../assets/img/gather_town_ex.png")}
+              style={styles.img}
+            />
           </View>
-          <View style={styles.profileRightContainer}>
-            <Text>nickname</Text>
-            <View style={styles.info}>
-              <Text>cotton candy 수강중</Text>
-              <Text>프리미엄 이용중</Text>
-            </View>
+
+          <View style={styles.bottom}>
+            <Text
+              style={{
+                fontFamily: "Poppins-Medium",
+                color: "white",
+                fontSize: 16,
+              }}
+            >
+              K-Culture with Influencers!
+            </Text>
+            <PremiumEnter />
           </View>
         </View>
-        <View>
-          <View>
-            <Text>현재 강의 수강 내역</Text>
+        <View style={styles.progress}>
+          <View style={styles.progressHeader}>
+            <Text style={styles.progressText}>Number of times tutored</Text>
+            <Text style={styles.progressText}>7/10</Text>
           </View>
-          <TouchableOpacity
-            activeOpacity={0.8}
-            style={styles.lecture}
-            onPress={() => {
-              navigation.navigate("Tutoring");
-            }}
-          >
-            <Text style={styles.levelText}>Level</Text>
-            <Text style={styles.progress}>70%</Text>
-            <View>
-              <Progress.Bar
-                progress={0.7}
-                height={20}
-                color={"#84E9FF"}
-                unfilledColor={"#C984FF"}
-                width={windowWidth * 0.8}
-              />
-            </View>
-          </TouchableOpacity>
+          <Progress.Bar
+            progress={0.7}
+            height={7}
+            color={"#A160E2"}
+            unfilledColor={"#F1EFF4"}
+            width={windowWidth - 40}
+            borderColor={"#F1EFF4"}
+          />
         </View>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -72,57 +85,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    paddingTop: Constants.statusBarHeight + 15,
+    paddingTop: Constants.statusBarHeight + 12,
     paddingHorizontal: 20,
   },
   header: {
+    marginBottom: 42,
+  },
+  courseContainer: {
+    backgroundColor: "#E6E3EA",
+    borderRadius: 9,
+    marginBottom: 16,
+  },
+  img: {
+    alignSelf: "center",
+  },
+  bottom: {
     flexDirection: "row",
-    justifyContent: "flex-end",
-    marginBottom: 20,
+    borderBottomLeftRadius: 9,
+    borderBottomRightRadius: 9,
+    backgroundColor: "rgba(68, 67, 69, 0.7)",
+    paddingVertical: 7,
+    paddingStart: 17,
+    justifyContent: "space-between",
+    paddingEnd: 9,
   },
-  content: {
-    // flex: 1,
-  },
-  profile: {
+  progressHeader: {
     flexDirection: "row",
-    marginBottom: 40,
-  },
-  profileLeftContainer: {
-    flexDirection: "column",
-    flex: 1,
-    alignItems: "center",
-  },
-  profileRightContainer: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  profileImg: {
-    width: 127,
-    height: 127,
-    borderRadius: 70,
-    backgroundColor: "#D9D9D9",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  info: {
-    height: 70,
-    backgroundColor: "#D9D9D9",
-    marginTop: 20,
-  },
-  levelText: {
-    fontSize: 16,
+    justifyContent: "space-between",
     marginBottom: 5,
-    marginTop: 5,
   },
-  progress: {
-    textAlign: "right",
-    marginRight: 20,
-  },
-  lecture: {
-    backgroundColor: "#D9D9D9",
-    height: 80,
-    paddingLeft: 10,
+  progressText: {
+    fontFamily: "Poppins-Medium",
+    color: "#A160E2",
+    fontSize: 10,
   },
 });
 
