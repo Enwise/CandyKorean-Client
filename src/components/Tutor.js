d93b4a66a9331f7e00eebb79bba3dfc2996fe00a
sohyun <kshyun0215@gmail.com>
2022-11-10 02:12:06 +0900

    튜터링 페이지 디자인 적용

--- src/components/Tutor.js
+++ src/components/Tutor.js
@@ -1,13 +1,21 @@
+import { useFonts } from "expo-font";
 import React from "react";
 import {
   Dimensions,
+  Platform,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
 const windowWidth = Dimensions.get("window").width;
-const Tutor = ({ onPress }) => {
+const Tutor = ({ onPress, disabled }) => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+  });
+  if (!fontsLoaded) return null;
   return (
     <View style={styles.container}>
       <View style={styles.profileImg}>
@@ -17,8 +25,8 @@ const Tutor = ({ onPress }) => {
         <Text style={styles.tutorName}>강사 이름</Text>
         <Text style={styles.tutorDescription}>강사 소개</Text>
       </View>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text>튜터링 받기</Text>
+      <TouchableOpacity style={styles.button(disabled)} onPress={onPress}>
+        <Text style={styles.buttonText}>Get tutored</Text>
       </TouchableOpacity>
     </View>
   );
@@ -26,39 +34,62 @@ const Tutor = ({ onPress }) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#D9D9D9",
     flexDirection: "row",
     padding: 10,
     width: windowWidth - 40,
-    marginBottom: 5,
+    marginBottom: 10,
   },
   profileImg: {
-    backgroundColor: "black",
-    width: 60,
-    height: 60,
-    borderRadius: 70,
+    backgroundColor: "white",
+    width: 51,
+    height: 51,
+    borderRadius: 51 / 2,
     justifyContent: "center",
     alignItems: "center",
+    ...Platform.select({
+      ios: {
+        shadowOpacity: 1,
+        shadowOffset: {
+          width: 0,
+          height: 0.1,
+        },
+        shadowRadius: 4.63,
+        shadowColor: "rgba(0, 0, 0, 0.07)",
+      },
+      android: {
+        elevation: 1,
+      },
+    }),
   },
   tutorProfile: {
-    marginLeft: 20,
+    marginLeft: 16,
     justifyContent: "center",
     flex: 2,
   },
   tutorName: {
-    fontSize: 16,
-    fontWeight: "bold",
+    fontSize: 14,
+    fontFamily: "Poppins-Medium",
+    color: "#444345",
   },
   tutorDescription: {
-    fontSize: 16,
-    marginTop: 10,
+    fontFamily: "Poppins-Regular",
+    color: "#B8B5BC",
+    fontSize: 10,
+    marginTop: 2,
   },
-  button: {
+  button: (disabled) => ({
+    backgroundColor: disabled ? "#B8B5BC" : "#A160E2",
     justifyContent: "center",
     alignItems: "center",
+    alignSelf: "center",
     flex: 1,
-    borderColor: "black",
-    borderWidth: 1,
+    borderRadius: 7,
+    height: 26,
+  }),
+  buttonText: {
+    color: "white",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 12,
   },
 });
 export default Tutor;
--- src/screens/Tutoring.js
+++ src/screens/Tutoring.js
@@ -1,41 +1,108 @@
 import React from "react";
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Constants from "expo-constants";
 import Tutor from "../components/Tutor";
 import AlertDialog from "../components/AlertDialog";
+import BackButton from "../components/BackButton";
+import { useFonts } from "expo-font";
+import TutoringHistory from "../components/TutoringHistory";
 const windowWidth = Dimensions.get("window").width;
-const Tutoring = () => {
+const windowHeight = Dimensions.get("window").height;
+const Tutoring = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+  });
+
   const [modalVisible, setModalVisible] = React.useState(false);
+
+  const ex_history = [
+    {
+      name: "Amy",
+      date: "09/02",
+      img: require("../assets/img/gather_town_ex.png"),
+    },
+    {
+      name: "Amy",
+      date: "09/07",
+      img: require("../assets/img/gather_town_ex.png"),
+    },
+    {
+      name: "Amy",
+      date: "09/12",
+      img: require("../assets/img/gather_town_ex.png"),
+    },
+    {
+      name: "Amy",
+      date: "09/19",
+      img: require("../assets/img/gather_town_ex.png"),
+    },
+  ];
+
+  if (!fontsLoaded) return null;
   return (
     <View style={styles.container}>
-      <View style={styles.gatherTownImg}>
-        <Image source={require("../assets/img/gather_town_ex.png")} />
+      <View style={styles.header}>
+        <BackButton onPress={() => navigation.pop()} />
       </View>
-      <View style={styles.content}>
-        <Text style={styles.classTitle}>Class Name</Text>
-        <Text>강의 설명</Text>
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.tutor}>tutors</Text>
-        <Tutor
-          onPress={() => {
-            setModalVisible(true);
-          }}
+      <ScrollView contentContainerStyle={{ paddingBottom: 30 }}>
+        <View style={{ paddingHorizontal: 20 }}>
+          <Text
+            style={{
+              fontFamily: "Poppins-SemiBold",
+              fontSize: 20,
+              marginTop: 42,
+            }}
+          >
+            K-Culture with Influencers!
+          </Text>
+          <View style={styles.gatherTownImg}>
+            <Image source={require("../assets/img/gather_town_ex.png")} />
+          </View>
+          <Text
+            style={{
+              fontFamily: "Poppins-Regular",
+              fontSize: 12,
+              color: "#807F82",
+            }}
+          >
+            K-Culture with Influencers -Culture {"\n"}with Influencers!
+          </Text>
+        </View>
+
+        <View style={styles.line}></View>
+        <View style={styles.content}>
+          <Text style={styles.title}>Tutors</Text>
+          <Tutor
+            onPress={() => {
+              setModalVisible(true);
+            }}
+          />
+          <Tutor
+            onPress={() => {
+              setModalVisible(true);
+            }}
+          />
+        </View>
+        <View style={styles.line2}></View>
+        <View style={styles.content}>
+          <Text style={styles.title}>Tutoring history</Text>
+          <TutoringHistory tutoring={ex_history} />
+        </View>
+        <AlertDialog
+          visible={modalVisible}
+          setModalVisible={setModalVisible}
+          url={"https://www.google.co.kr"}
         />
-        <Tutor
-          onPress={() => {
-            setModalVisible(true);
-          }}
-        />
-      </View>
-      <View style={styles.content}>
-        <Text>튜터링 내역</Text>
-      </View>
-      <AlertDialog
-        visible={modalVisible}
-        setModalVisible={setModalVisible}
-        url={"https://www.google.co.kr"}
-      />
+      </ScrollView>
     </View>
   );
 };
@@ -43,10 +110,13 @@ const Tutoring = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Constants.statusBarHeight + 15,
-    paddingHorizontal: 20,
-    alignItems: "center",
     backgroundColor: "white",
+  },
+  header: {
+    marginTop: Constants.statusBarHeight + 12,
+    width: 24,
+    height: 24,
+    paddingHorizontal: 20,
   },
   gatherTownImg: {
     backgroundColor: "#D9D9D9",
@@ -54,18 +124,35 @@ const styles = StyleSheet.create({
     height: 200,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: 9,
+    marginTop: 5,
+    marginBottom: 10,
   },
   content: {
     alignSelf: "flex-start",
     marginTop: 20,
+    paddingHorizontal: 20,
   },
   classTitle: {
     fontSize: 20,
     marginBottom: 10,
   },
-  tutor: {
-    fontSize: 20,
-    marginBottom: 10,
+  title: {
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 16,
+    color: "#807F82",
+    marginBottom: 20,
+  },
+  line: {
+    backgroundColor: "#F1EFF4",
+    height: 4,
+    marginTop: 25,
+  },
+  line2: {
+    borderWidth: 0.5,
+    borderColor: "#F1EFF4",
+    marginBottom: 25,
+    marginTop: 25,
   },
 });
 export default Tutoring;
