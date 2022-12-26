58b70a58f0de232a660df2aeb525e1510d4522b4
ahnwooseok <peterdntjr@naver.com>
2022-12-27 07:28:17 +0900

    닉네임 변경 로직 변경

--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -134,7 +134,7 @@ const Setting = ({navigation}) => {
                                 style={{width:200, height:200, borderRadius:100}}
                             />
                         </View>
-                        <TextInput
+                        <TextInput         
                             style={styles.input}
                             onChangeText={setNickname}
                             value={nickname}
